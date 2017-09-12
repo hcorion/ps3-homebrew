f6b6e57d54edee70de3482bb2343d58980bcf5be
Zion Nimchuk <zionnimchuk@gmail.com>
2017-09-11 19:15:14 -0700

    Finalize ssl Cert test

--- ssl_certificate_test/source/main.c
+++ ssl_certificate_test/source/main.c
@@ -20,18 +20,19 @@
 uint8_t httpPool[HTTP_POOL_SIZE];
 uint8_t sslPool[SSL_POOL_SIZE];
 
-int loadCert(uint32_t size, uint64_t flag)
+int loadCert(uint64_t flag)
 {
-	uint32_t sized;
-	sslCertificateLoader(CELL_SSL_LOAD_CERT_ALL, NULL, 0, &sized);
-	char* buf = (char*)malloc(sized);
+	uint32_t size;
+	sslCertificateLoader(flag, NULL, 0, &size);
+	char* buf = (char*)malloc(size);
+	printf("size: %"PRIu32"\n", size);
 	//memset(buf, '\0', size);
-	int ret = sslCertificateLoader(flag, buf, (uint32_t)sized, NULL);
+	int ret = sslCertificateLoader(flag, buf, (uint32_t)size, NULL);
 	if (ret != 0)
 	{
 		printf("sslCertificateLoader failed: %d\n", ret);
 	}
-	char word[sized];
+	char word[size];
 	strcpy(word, buf);
 	printf("%s\n", word);
 	printf("-----------next-----------\n");
@@ -72,61 +73,100 @@ int main(int argc,char *argv[])
 	}
 	
 	// Time for certificates
-	//117440519
-	// Motorstorm uses 117440519
 	printf("CELL_SSL_LOAD_CERT_NORMAL: %llu\n", CELL_SSL_LOAD_CERT_NORMAL);
 	printf("CELL_SSL_LOAD_CERT_ALL: %llu\n", CELL_SSL_LOAD_CERT_ALL);
 	
 	httpsData *caList = NULL;
-	uint32_t size = 0;
 	
-	//ret = sslCertificateLoader(CELL_SSL_LOAD_CERT_ALL, NULL, 0, &size);
-	printf("ret: %d\n", ret);
-	printf("size: %"PRIu32"\n", size);
-	//printf("sizeof(uint32_t): %"PRIu32"\n", sizeof(uint32_t));
-	//printf("sizeof(size_t): %"PRIu32"\n", sizeof(size_t));
+	printf("CELL_SSL_LOAD_CERT_SCE01\n");
+	loadCert(CELL_SSL_LOAD_CERT_SCE01);
+	printf("CELL_SSL_LOAD_CERT_SCE02\n");
+	loadCert(CELL_SSL_LOAD_CERT_SCE02);
+	printf("CELL_SSL_LOAD_CERT_SCE03\n");
+	loadCert(CELL_SSL_LOAD_CERT_SCE03);
+	printf("CELL_SSL_LOAD_CERT_SCE04\n");
+	loadCert(CELL_SSL_LOAD_CERT_SCE04);
+	printf("CELL_SSL_LOAD_CERT_SCE05\n");
+	loadCert(CELL_SSL_LOAD_CERT_SCE05);
+	printf("CELL_SSL_LOAD_CERT_NORMAL\n");
+	loadCert(CELL_SSL_LOAD_CERT_NORMAL);
+	printf("CELL_SSL_LOAD_CERT_CLASS1_PCA_G2_V2\n");
+	loadCert(CELL_SSL_LOAD_CERT_CLASS1_PCA_G2_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS1_PCA_G3_V2\n");
+	loadCert(CELL_SSL_LOAD_CERT_CLASS1_PCA_G3_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS1_PCA_SS_V4\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS1_PCA_SS_V4);
+	printf("CELL_SSL_LOAD_CERT_CLASS2_PCA_G2_V2\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS2_PCA_G2_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS2_PCA_G3_V2\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS2_PCA_G3_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS2_PCA_SS_V4\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS2_PCA_SS_V4);
+	printf("CELL_SSL_LOAD_CERT_CLASS3_PCA_G2_V2\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS3_PCA_G2_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS3_PCA_G3_V2\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS3_PCA_G3_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS3_PCA_SS_V4\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS3_PCA_SS_V4);
+	printf("CELL_SSL_LOAD_CERT_CLASS4_PCA_G2_V2\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS4_PCA_G2_V2);
+	printf("CELL_SSL_LOAD_CERT_CLASS4_PCA_G3_V2\n");
+ 	loadCert(CELL_SSL_LOAD_CERT_CLASS4_PCA_G3_V2);
+	printf("CELL_SSL_LOAD_CERT_ENTRUST_NET_SS_CA\n");
+	loadCert(CELL_SSL_LOAD_CERT_ENTRUST_NET_SS_CA);
+	printf("CELL_SSL_LOAD_CERT_EQUIFAX_SEC_CA\n");
+	loadCert(CELL_SSL_LOAD_CERT_EQUIFAX_SEC_CA);
+	printf("CELL_SSL_LOAD_CERT_EQUIFAX_SEC_EBUS1\n");
+	loadCert(CELL_SSL_LOAD_CERT_EQUIFAX_SEC_EBUS1);
+	printf("CELL_SSL_LOAD_CERT_GEOTRUST_GCA\n");
+	loadCert(CELL_SSL_LOAD_CERT_GEOTRUST_GCA);
+	printf("CELL_SSL_LOAD_CERT_GLOBALSIGN_RCA\n");
+	loadCert(CELL_SSL_LOAD_CERT_GLOBALSIGN_RCA);
+	printf("CELL_SSL_LOAD_CERT_GTE_CT_G_RCA\n");
+	loadCert(CELL_SSL_LOAD_CERT_GTE_CT_G_RCA);
+	printf("CELL_SSL_LOAD_CERT_GTE_CT_ROOT\n");
+	loadCert(CELL_SSL_LOAD_CERT_GTE_CT_ROOT);
+	printf("CELL_SSL_LOAD_CERT_RSA1024_V1\n");
+	loadCert(CELL_SSL_LOAD_CERT_RSA1024_V1);
+	printf("CELL_SSL_LOAD_CERT_RSA2048_V3\n");
+	loadCert(CELL_SSL_LOAD_CERT_RSA2048_V3);
+	printf("CELL_SSL_LOAD_CERT_RSA_SECURE_SERVER\n");
+	loadCert(CELL_SSL_LOAD_CERT_RSA_SECURE_SERVER);
+	printf("CELL_SSL_LOAD_CERT_THAWTE_PREM_SCA\n");
+	loadCert(CELL_SSL_LOAD_CERT_THAWTE_PREM_SCA);
+	printf("CELL_SSL_LOAD_CERT_THAWTE_SCA\n");
+	loadCert(CELL_SSL_LOAD_CERT_THAWTE_SCA );
+	printf("CELL_SSL_LOAD_CERT_VALICERT_CLASS2\n");
+	loadCert(CELL_SSL_LOAD_CERT_VALICERT_CLASS2);
+	printf("CELL_SSL_LOAD_CERT_VERISIGN_TSA_CA\n");
+	loadCert(CELL_SSL_LOAD_CERT_VERISIGN_TSA_CA);
+	printf("CELL_SSL_LOAD_CERT_AAA_CERT_SERVICES\n");
+	loadCert(CELL_SSL_LOAD_CERT_AAA_CERT_SERVICES);
+	printf("CELL_SSL_LOAD_CERT_ADDTRUST_EXT_CA\n");
+	loadCert(CELL_SSL_LOAD_CERT_ADDTRUST_EXT_CA);
+	printf("CELL_SSL_LOAD_CERT_UTN_USERFIRST_HW\n");
+	loadCert(CELL_SSL_LOAD_CERT_UTN_USERFIRST_HW);
+	printf("CELL_SSL_LOAD_CERT_EQUIFAX_SEC_G_EBUS1\n");
+	loadCert(CELL_SSL_LOAD_CERT_EQUIFAX_SEC_G_EBUS1);
+	printf("CELL_SSL_LOAD_CERT_CLASS3_PCA_G5\n");
+	loadCert(CELL_SSL_LOAD_CERT_CLASS3_PCA_G5);
+	printf("CELL_SSL_LOAD_CERT_SHA256\n");
+	loadCert(CELL_SSL_LOAD_CERT_SHA256);
+	printf("The Last of Us cert\n");
+	// The Last of Us cert.
+	loadCert(562948804079679);
+	printf("MotorStorm Apocalypse cert\n");
+	// MotorStorm Apocalypse cert.
+	loadCert(117440519);
+	printf("CELL_SSL_LOAD_CERT_NOT_FOR_SERVER\n");
+	loadCert(CELL_SSL_LOAD_CERT_NOT_FOR_SERVER);
+	printf("CELL_SSL_LOAD_CERT_OBSOLETE\n");
+	loadCert(CELL_SSL_LOAD_CERT_OBSOLETE);
 	
-	//loadCert(size, CELL_SSL_LOAD_CERT_SCE01);
-	//loadCert(size, CELL_SSL_LOAD_CERT_SCE02);
-	//loadCert(size, CELL_SSL_LOAD_CERT_SCE03);
-	//loadCert(size, CELL_SSL_LOAD_CERT_SCE04);
-	//loadCert(size, CELL_SSL_LOAD_CERT_SCE05);
-// 	//loadCert(size, CELL_SSL_LOAD_CERT_NORMAL);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS1_PCA_G2_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS1_PCA_G3_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS1_PCA_SS_V4);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS2_PCA_G2_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS2_PCA_G3_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS2_PCA_SS_V4);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS3_PCA_G2_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS3_PCA_G3_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS3_PCA_SS_V4);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS4_PCA_G2_V2);
-// 	loadCert(size, CELL_SSL_LOAD_CERT_CLASS4_PCA_G3_V2);
-	loadCert(size, CELL_SSL_LOAD_CERT_ENTRUST_NET_SS_CA);
-	loadCert(size, CELL_SSL_LOAD_CERT_EQUIFAX_SEC_CA);
-	loadCert(size, CELL_SSL_LOAD_CERT_EQUIFAX_SEC_EBUS1);
-	loadCert(size, CELL_SSL_LOAD_CERT_GEOTRUST_GCA);
-	loadCert(size, CELL_SSL_LOAD_CERT_GLOBALSIGN_RCA);
-	loadCert(size, CELL_SSL_LOAD_CERT_GTE_CT_G_RCA);
-	loadCert(size, CELL_SSL_LOAD_CERT_GTE_CT_ROOT);
-	loadCert(size, CELL_SSL_LOAD_CERT_RSA1024_V1);
-	loadCert(size, CELL_SSL_LOAD_CERT_RSA2048_V3);
-	loadCert(size, CELL_SSL_LOAD_CERT_RSA_SECURE_SERVER);
-	loadCert(size, CELL_SSL_LOAD_CERT_THAWTE_PREM_SCA);
-	loadCert(size, CELL_SSL_LOAD_CERT_THAWTE_SCA );
-	loadCert(size, CELL_SSL_LOAD_CERT_VALICERT_CLASS2);
-	loadCert(size, CELL_SSL_LOAD_CERT_VERISIGN_TSA_CA);
-	loadCert(size, CELL_SSL_LOAD_CERT_AAA_CERT_SERVICES);
-	loadCert(size, CELL_SSL_LOAD_CERT_ADDTRUST_EXT_CA);
-	loadCert(size, CELL_SSL_LOAD_CERT_UTN_USERFIRST_HW);
-	loadCert(size, CELL_SSL_LOAD_CERT_EQUIFAX_SEC_G_EBUS1);
-	loadCert(size, CELL_SSL_LOAD_CERT_CLASS3_PCA_G5);
-	loadCert(size, CELL_SSL_LOAD_CERT_SHA256);
 	
 	//Invalid cert tests
 	
-	ret = loadCert(size, 0);
+	/*ret = loadCert(0);
 	void* buf = malloc(size);
 	ret = sslCertificateLoader(0, buf, size, NULL);
 	printf("loadCert 0: %d\n", ret);
@@ -137,7 +177,7 @@ int main(int argc,char *argv[])
 	ret = sslCertificateLoader(0x0800000000000000ULL, buf3, size, NULL);
 	printf("loadCert 0x0800000000000000: %d\n", ret);
 	ret = sslCertificateLoader(CELL_SSL_LOAD_CERT_ALL << 1, NULL, 0, &size);
-	printf("loadCert CELL_SSL_LOAD_CERT_ALL << 1: %d\n", ret);
+	printf("loadCert CELL_SSL_LOAD_CERT_ALL << 1: %d\n", ret);*/
 
 	return 0;
 }
