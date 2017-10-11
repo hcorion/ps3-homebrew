a05c56fccaed3ab15b92207e1c60b22439f33f2d
Zion Nimchuk <zionnimchuk@gmail.com>
2017-10-10 22:29:11 -0700

    usbd work

--- usbd_test/source/main.c
+++ usbd_test/source/main.c
@@ -8,51 +8,66 @@
 #include <stdio.h>
 int main()
 {
+	sysModuleLoad(SYSMODULE_USB);
+	
+	//////////
+	// Init //
+	//////////
+	unsigned int uuid = 0;
+	sys_usbd_initialize(&uuid);
 	// sys_usbd_initiallize returns: 
 	//805322496
 	//805308161
-	sysModuleLoad(SYSMODULE_USB);
-	// Init
-	unsigned int uuid = 0;
-	sys_usbd_initialize(&uuid);
-	// Get device list
+	
+	/////////////////////
+	// Get device list //
+	/////////////////////
 	char* funkyaddr = malloc (50);
-	printf("0x%x\n", funkyaddr);
+	printf("device_list addr: 0x%x\n", funkyaddr);
 	char thatchar = 0x7f;
 	int numOfDevices = sys_usbd_get_device_list(uuid, funkyaddr, thatchar);
+	
 	printf("Number of devices: %d\n", numOfDevices);
 	
-	for (int i = 0; i < 50; i++)
+	int crazyman = 0;
+	while (crazyman < 50)
 	{
-		unsigned char* x = funkyaddr + (i);
-		printf("0x%x\n", *x);
+		char word[9];
+		int length = 0;
+		for (int i = 0; i < 4; i++)
+		{
+			
+			unsigned char* x = funkyaddr + (crazyman);
+			crazyman += 1;
+			length += sprintf(word + length, "0x%x ", *x);
+		}
+		// If there is no data, don't bother printing it.
+		if (strcmp(word, "0x0 0x0 0x0 0x0 "))
+		{
+			printf("%s\n", word);
+		}
 	}
 	free(funkyaddr);
-	// Descriptor test
-	unsigned int unk1 = 65538;
+	
+	
+	//////////////////////
+	// Descriptor tests //
+	//////////////////////
+	
+	char unk1 = 3;
 	int descSize = sys_usbd_get_descriptor_size(uuid, unk1);
 	// Returns 50, unk1 doesn't change.
-	printf("unk1=%d, descSize=%d\n", unk1, descSize);
+	printf("descriptor_size: unk1=%d, descSize=%d\n", unk1, descSize);
 	// time to actually get the descriptor
-	char* descriptor = malloc (50);
-	printf("0x%x\n", funkyaddr);
-	/*
-	 * 00 00 00 00 d0 08 0a 48 32 0d c1 a8 00 00 00 81 
-20 01 00 b4 20 01 00 b4 00 00 00 00 00 00 00 80 
-00 00 00 00 00 00 01 00 00 00 00 00 00 00 01 00 
-00 00
-	 */
-	//descriptor[4] = 0xd0;
-	//descriptor[5] = 0x08;
-	//descriptor[6] = 0x0A;
-	//descriptor[7] = 0x48;
-	char array[50] = {0x00, 0x00, 0x00, 0x00, 0xd0, 0x08, 0x0a, 0x48, 0x32, 0x0d, 0xc1, 0xa8, 0x00, 0x00, 0x00, 0x81, 
-0x20, 0x01, 0x00, 0xb4, 0x20, 0x01, 0x00, 0xb4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 
-0x00, 0x00};
-	memcpy(descriptor, array, 50);
+	char* descriptor = malloc (descSize);
+	printf("Descriptor: 0x%x\n", descriptor);
+	/*char array[50] = {0x00, 0x00, 0x00, 0x00, 0xd0, 0x08, 0x0a, 0x48, 0x32, 0x0d, 0xc1, 0xa8, 0x00, 0x00, 0x00, 0x81, 
+	0x20, 0x01, 0x00, 0xb4, 0x20, 0x01, 0x00, 0xb4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 
+	0x00, 0x00};*/
+	//memcpy(descriptor, array, 50);
 	int ret = sys_usbd_get_descriptor(uuid, unk1, descriptor, descSize);
-	printf("unk1=%d, descSize=%d ret=%d\n", unk1, descSize, ret);
+	printf("get_descriptor_size: unk1=%d, descSize=%d ret=%d\n", unk1, descSize, ret);
 	for (int i = 0; i < 50; i++)
 	{
 		unsigned char* x = descriptor + (i);
